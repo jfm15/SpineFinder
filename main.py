069ea17298b2b99f48aa26c8ad0891cc5e3cd703
jfm15 <jfm15@imperial.ac.uk>
2019-04-29 21:48:04 +0100

    change net

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
 
 model.add(Conv3D(64, kernel_size=(3, 3, 3), strides=(1, 1, 1), activation='relu', padding="same"))
 
-model.add(Conv3D(1024, kernel_size=(8, 8, 2), strides=(1, 1, 1), activation='relu'))
+model.add(Conv3D(1024, kernel_size=(8, 8, 2), strides=(8, 8, 2), activation='relu'))
 
 model.add(Conv3D(3, kernel_size=(1, 1, 1), strides=(1, 1, 1)))
 # output_shape=(1, 1, 1, 3)
