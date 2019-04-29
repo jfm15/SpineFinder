39de60b695abbe75d52bbd1d841b606a9714a8b2
Ubuntu <jfm15@VM-SpineFinder.clqtktuwrjdudamghtk1jwahjc.ax.internal.cloudapp.net>
2019-04-29 20:02:04 +0000

    remtoe changes

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ validation_generator = DataGenerator(partition['validation'], labels, **params)
 
 # train model
 model = Sequential()
-#model.add(Conv3D(64, kernel_size=(5, 5, 3), strides=(1, 1, 1), activation='relu', padding="same",
-                 #input_shape=(None, None, None, 1)))
+model.add(Conv3D(64, kernel_size=(5, 5, 3), strides=(2, 2, 2), activation='relu', padding="same",
+                 input_shape=(None, None, None, 1)))
 
 model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
 
@@ -42,7 +42,7 @@ model.add(Conv3D(64, kernel_size=(3, 3, 3), strides=(1, 1, 1), activation='relu'
 
 model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2)))
 
-model.add(Conv3D(1024, kernel_size=(8, 8, 1), strides=(1, 1, 1), activation='relu'))
+model.add(Conv3D(1024, kernel_size=(4, 4, 1), strides=(1, 1, 1), activation='relu'))
 
 model.add(Conv3D(3, kernel_size=(1, 1, 1), strides=(1, 1, 1)))
 # output_shape=(1, 1, 1, 3)
@@ -61,7 +61,7 @@ model.fit_generator(generator=training_generator,
                     validation_data=validation_generator,
                     use_multiprocessing=True,
                     workers=6,
-                    epochs=10)
+                    epochs=3)
 
 model.save('main-model.h5')
 
