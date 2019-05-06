cefa9dd5b16d7c32bb3b532c7937fd71c12cf28e
jfm15 <jfm15@imperial.ac.uk>
2019-05-06 14:08:14 +0100

    more complicated network

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ model.fit_generator(generator=training_generator,
                     validation_data=validation_generator,
                     use_multiprocessing=True,
                     workers=6,
-                    epochs=5)
+                    epochs=20)
 
 model.save('main-model.h5')
 
