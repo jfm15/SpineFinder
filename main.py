406b9c4ea71df635ebc32efb1cf50bd9a3694f47
jfm15 <jfm15@imperial.ac.uk>
2019-05-08 00:28:21 +0100

    zero samples and old network

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ model.fit_generator(generator=training_generator,
                     validation_data=validation_generator,
                     use_multiprocessing=True,
                     workers=6,
-                    epochs=10)
+                    epochs=20)
 
 model.save('main-model.h5')
 
