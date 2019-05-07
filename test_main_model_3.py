b3a496048773ed9e3006226a18ea432d4b111708
jfm15 <jfm15@imperial.ac.uk>
2019-05-07 01:22:24 +0100

    test model script

--- test_main_model_3.py
+++ test_main_model_3.py
@@ -1,25 +1,28 @@
 import numpy as np
 from keras.models import load_model
-import keras.losses
 from keras_weighted_categorical_crossentropy import weighted_categorical_crossentropy
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 path = 'samples/2551924-34'
 sample = np.load(path + '-sample.npy')
 sample_labelling = np.load(path + '-labelling.npy')
 
-weights = np.ones(27)
-weights[0] = 0.001
-weights /= np.sum(weights)
+weights = np.array([0.1, 0.9])
 
 model = load_model('main-model.h5', custom_objects={'loss': weighted_categorical_crossentropy(weights)})
 
 result = model.predict(sample.reshape(1, 28, 28, 28, 1))
-result = result.reshape(28, 28, 28, 27)
-print(np.around(result[5, 12, 21], decimals=2))
-i, j, k = np.where(sample_labelling > 0)
-print(i, j, k)
-print(sample_labelling[21, 12, 5])
+result = result.reshape(28, 28, 28, 2)
 decat_result = np.argmax(result, axis=3)
+
+labelling = decat_result[20, :, :]
+
+masked_data = np.ma.masked_where(labelling == 0, labelling)
+
+plt.imshow(sample[20, :, :], interpolation="none", origin='lower')
+plt.imshow(masked_data, interpolation="none", origin='lower', cmap=cm.jet, alpha=0.5)
+plt.show()
 
 print(np.bincount(sample_labelling.reshape(-1).astype(int)))
 print(np.bincount(decat_result.reshape(-1).astype(int)))
---
+++ test_main_model_4.py
@@ -0,0 +1,39 @@
+import numpy as np
+from keras.models import load_model
+from keras_weighted_categorical_crossentropy import weighted_categorical_crossentropy
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from utility_functions import opening_files
+
+volume = opening_files.read_nii("datasets/spine-1/patient0088/2684937/2684937.nii.gz")
+
+weights = np.array([0.1, 0.9])
+
+model = load_model('main-model.h5', custom_objects={'loss': weighted_categorical_crossentropy(weights)})
+
+output = np.zeros(volume.shape)
+
+for x in range(0, volume.shape[0] - 28, 28):
+    for y in range(0, volume.shape[1] - 28, 28):
+        for z in range(0, volume.shape[2] - 28, 28):
+            corner_a = [x, y, z]
+            corner_b = [x + 28, y + 28, z + 28]
+            patch = volume[corner_a[0]:corner_b[0], corner_a[1]:corner_b[1], corner_a[2]:corner_b[2]]
+            result = model.predict(patch.reshape(1, 28, 28, 28, 1))
+            result = result.reshape(28, 28, 28, 2)
+            decat_result = np.argmax(result, axis=3)
+            output[corner_a[0]:corner_b[0], corner_a[1]:corner_b[1], corner_a[2]:corner_b[2]] = decat_result
+            print(x, y, z, np.bincount(decat_result.reshape(-1).astype(int)))
+
+
+print(volume.shape)
+volume_slice = volume[35, :, :]
+predict_slice = output[35, :, :]
+
+masked_data = np.ma.masked_where(predict_slice == 0, predict_slice)
+
+plt.imshow(volume_slice.T, interpolation="none", origin='lower')
+plt.imshow(masked_data.T, interpolation="none", origin='lower', cmap=cm.jet, alpha=0.5)
+plt.show()
+
+
