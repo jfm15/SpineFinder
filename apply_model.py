fc8bd1185ca019388e9d1743702969e57bada7bb
jfm15 <jfm15@imperial.ac.uk>
2019-05-07 15:21:01 +0100

    easy way to visualise results

--- apply_model.py
+++ apply_model.py
@@ -1,41 +1,42 @@
 import numpy as np
 from keras.models import load_model
 from losses_and_metrics.keras_weighted_categorical_crossentropy import weighted_categorical_crossentropy
-import matplotlib.pyplot as plt
 from utility_functions import opening_files
 
-volume = opening_files.read_nii("datasets/spine-1/patient0088/2684937/2684937.nii.gz")
+
+def apply_model(data_path, model_dir, prediction_dir, patch_size, custom_objects={}, file_ext=".nii.gz"):
+
+    volume = opening_files.read_nii(data_path)
+
+    model = load_model(model_dir, custom_objects=custom_objects)
+
+    ext_len = len(file_ext)
+    name = (data_path.rsplit('/', 1)[-1])[:-ext_len]
+    name = name + "-prediction"
+
+    output = np.zeros(volume.shape)
+
+    for x in range(0, volume.shape[0] - patch_size[0], patch_size[0]):
+        for y in range(0, volume.shape[1] - patch_size[1], patch_size[1]):
+            for z in range(0, volume.shape[2] - patch_size[2], patch_size[2]):
+                corner_a = [x, y, z]
+                corner_b = corner_a + patch_size
+                patch = volume[corner_a[0]:corner_b[0], corner_a[1]:corner_b[1], corner_a[2]:corner_b[2]]
+                patch = np.expand_dims(patch, axis=0)
+                patch = np.expand_dims(patch, axis=4)
+                result = model.predict(patch)
+                result = np.squeeze(result, axis=0)
+                decat_result = np.argmax(result, axis=3)
+                output[corner_a[0]:corner_b[0], corner_a[1]:corner_b[1], corner_a[2]:corner_b[2]] = decat_result
+                print(x, y, z, np.bincount(decat_result.reshape(-1).astype(int)))
+
+    prediction_path = '/'.join([prediction_dir, name])
+    np.save(prediction_path, output)
+
 
 weights = np.array([0.1, 0.9])
-model = load_model('model_files/main-model.h5', custom_objects={'loss': weighted_categorical_crossentropy(weights)})
-
-output = np.zeros(volume.shape)
-
-patch_size = np.array([28, 28, 28])
-
-for x in range(0, volume.shape[0] - patch_size[0], patch_size[0]):
-    for y in range(0, volume.shape[1] - patch_size[1], patch_size[1]):
-        for z in range(0, volume.shape[2] - patch_size[2], patch_size[2]):
-            corner_a = [x, y, z]
-            corner_b = corner_a + patch_size
-            patch = volume[corner_a[0]:corner_b[0], corner_a[1]:corner_b[1], corner_a[2]:corner_b[2]]
-            patch = np.expand_dims(patch, axis=0)
-            patch = np.expand_dims(patch, axis=4)
-            result = model.predict(patch)
-            result = np.squeeze(result, axis=0)
-            decat_result = np.argmax(result, axis=3)
-            output[corner_a[0]:corner_b[0], corner_a[1]:corner_b[1], corner_a[2]:corner_b[2]] = decat_result
-            print(x, y, z, np.bincount(decat_result.reshape(-1).astype(int)))
-
-
-print(volume.shape)
-volume_slice = volume[35, :, :]
-predict_slice = output[35, :, :]
-
-masked_data = np.ma.masked_where(predict_slice == 0, predict_slice)
-
-plt.imshow(volume_slice.T, interpolation="none", origin='lower')
-plt.imshow(masked_data.T, interpolation="none", origin='lower', alpha=0.5)
-plt.show()
-
-
+apply_model(data_path="datasets/spine-1/patient0088/2684937/2684937.nii.gz",
+            model_dir="model_files/main-model.h5",
+            prediction_dir="predictions",
+            patch_size=np.array([28, 28, 28]),
+            custom_objects={'loss': weighted_categorical_crossentropy(weights)})
---
+++ visualise_results.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from utility_functions import opening_files
+
+
+def visualise_results(data_path, prediction_path, plot_dir, cut_range=(0.2, 0.8), rows=1, cols=6, file_ext="-prediction.npy"):
+
+    volume = opening_files.read_nii(data_path)
+    prediction = np.load(prediction_path)
+
+    fig, axs = plt.subplots(nrows=rows, ncols=cols, figsize=(9.3, 6))
+
+    fig.subplots_adjust(left=0.03, right=0.97, hspace=0.3, wspace=0.05)
+
+    lower = volume.shape[0] * cut_range[0]
+    upper = volume.shape[0] * cut_range[1]
+    cut_indices = np.round(np.linspace(lower, upper, num=rows*cols)).astype(int)
+
+    for ax, cut_idx in zip(axs.flat, cut_indices):
+        volume_slice = volume[cut_idx, :, :]
+        prediction_slice = prediction[cut_idx, :, :]
+
+        ax.imshow(volume_slice.T, vmin=-1000, vmax=1000)
+
+        if np.unique(prediction_slice).shape[0] > 1:
+            masked_data = np.ma.masked_where(prediction_slice == 0, prediction_slice)
+            ax.imshow(masked_data.T, cmap=cm.spring, alpha=0.5)
+
+    plt.tight_layout()
+
+    ext_len = len(file_ext)
+    name = (prediction_path.rsplit('/', 1)[-1])[:-ext_len]
+    name = name + "-plots"
+    plot_path = '/'.join([plot_dir, name])
+    plt.savefig(plot_path)
+
+
+visualise_results(data_path="datasets/spine-1/patient0088/2684937/2684937.nii.gz",
+                  prediction_path="predictions/2684937-prediction.npy",
+                  plot_dir="plots")
