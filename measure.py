8b059489b18a27eaea6a788303c1dbd8c3c0f74c
jfm15 <jfm15@imperial.ac.uk>
2019-05-13 17:23:34 +0100

    debug

--- measure.py
+++ measure.py
@@ -80,7 +80,7 @@ def test_scan(scan_path, centroid_path, detection_model_path, detection_model_in
     print(detections.shape, np.unique(detections))
     # get the largest island
     # bounds, detections = sampling_helper_functions.crop_labelling(detections)
-    largest_island_np = np.nonzero(detections)
+    largest_island_np = np.transpose(np.nonzero(detections))
     i_min = np.min(largest_island_np[:, 0])
     i_max = np.max(largest_island_np[:, 0])
     j_min = np.min(largest_island_np[:, 1])
