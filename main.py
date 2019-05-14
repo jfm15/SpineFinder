beaa6d0f426ecb931f2f69ce92b4ebc8505c69f9
jfm15 <jfm15@imperial.ac.uk>
2019-05-14 14:59:20 +0100

    change weights

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ model = unet(input_shape=(28, 28, 28),
 
 model = six_conv_two_classes(input_shape=(30, 30, 60, 1),
                              kernel_size=(3, 3, 3),
-                             weights=np.array([0.1, 0.9]))
+                             weights=np.array([0.05, 0.95]))
 
 '''
 weights = np.ones(28) * 0.0384
