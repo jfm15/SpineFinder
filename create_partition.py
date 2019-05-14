9c8ebf4ea02870712bf1253792259ab6ecf6688b
jfm15 <jfm15@imperial.ac.uk>
2019-05-14 22:59:33 +0100

    don't have training and validation samples from the same scan

--- create_partition.py
+++ create_partition.py
@@ -1,12 +1,8 @@
-import glob, os
+import glob
 import numpy as np
 
-def create_partition_and_labels(samples_dir, training_percentage, randomise=True):
 
-    label_translation = ["B", "C1", "C2", "C3", "C4", "C5", "C6", "C7",
-                         "T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9",
-                         "T10", "T11", "T12", "L1", "L2", "L3", "L4", "L5", "L6",
-                         "S1", "S2"]
+def create_partition_and_labels(samples_dir, training_percentage, randomise=True):
 
     partition = {}
     training_labels = []
@@ -15,24 +11,33 @@ def create_partition_and_labels(samples_dir, training_percentage, randomise=True
 
     ext_len = len("-labelling.npy")
     paths = glob.glob(samples_dir + "/**/*labelling.npy", recursive=True)
-    no_of_training = round(len(paths) * training_percentage)
+    scans = list(map(lambda x: x.split('-')[0], paths))
+    scans = np.unique(np.array(scans))
+    no_of_training_scans = round(scans.shape[0] * training_percentage)
 
     if randomise:
-        np.random.shuffle(paths)
+        np.random.shuffle(scans)
 
-    for i, label_path in enumerate(paths):
+    add_to_training = True
 
-        sample_path_without_ext = label_path[:-ext_len]
-        label = sample_path_without_ext.rsplit('/', 1)[1]
+    for i, scan_path in enumerate(scans):
 
-        # assign to lists for partition
-        if i < no_of_training:
-            training_labels.append(label)
-        else:
-            validation_labels.append(label)
+        if i > no_of_training_scans:
+            add_to_training = False
 
-        # read file and assign to labels
-        labels[label] = label + "-labelling"
+        for label_path in glob.glob(scan_path + "*-labelling.npy", recursive=True):
+
+            sample_path_without_ext = label_path[:-ext_len]
+            label = sample_path_without_ext.rsplit('/', 1)[1]
+
+            # assign to lists for partition
+            if add_to_training:
+                training_labels.append(label)
+            else:
+                validation_labels.append(label)
+
+            # read file and assign to labels
+            labels[label] = label + "-labelling"
 
     partition["train"] = training_labels
     partition["validation"] = validation_labels
