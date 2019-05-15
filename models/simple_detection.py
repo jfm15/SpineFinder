5a1321969d98080a902759b7b40ede1421c26ff4
jfm15 <jfm15@imperial.ac.uk>
2019-05-15 15:34:51 +0100

    remove rotation

--- models/simple_detection.py
+++ models/simple_detection.py
@@ -3,7 +3,7 @@ import keras_metrics as km
 import keras.metrics as metrics
 from keras import optimizers
 from keras.models import Sequential
-from keras.layers import Conv3D
+from keras.layers import Conv3D, BatchNormalization
 from losses_and_metrics.keras_weighted_categorical_crossentropy import weighted_categorical_crossentropy
 from losses_and_metrics.dsc import dice_coef_label
 
@@ -13,13 +13,16 @@ def six_conv_two_classes(input_shape, kernel_size, weights):
     model = Sequential()
     model.add(Conv3D(16, kernel_size=kernel_size, strides=(1, 1, 1), activation='relu', padding="same",
                      input_shape=input_shape))
+    model.add(BatchNormalization())
     model.add(Conv3D(16, kernel_size=kernel_size, strides=(1, 1, 1), activation='relu', padding="same"))
+    model.add(BatchNormalization())
     model.add(Conv3D(16, kernel_size=kernel_size, strides=(1, 1, 1), activation='relu', padding="same"))
+    model.add(BatchNormalization())
     model.add(Conv3D(2, kernel_size=kernel_size, strides=(1, 1, 1), activation='softmax', padding="same"))
 
     # define optimizer
     # sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    adam = optimizers.Adam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
+    adam = optimizers.Adam(lr=0.02, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
 
     # define loss function
     loss_function = weighted_categorical_crossentropy(weights)
