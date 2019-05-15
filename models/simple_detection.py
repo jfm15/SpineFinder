2bed67337991ff56b73d75b80bf47d111277e0df
jfm15 <jfm15@imperial.ac.uk>
2019-05-15 16:22:34 +0100

    go back to old model

--- models/simple_detection.py
+++ models/simple_detection.py
@@ -3,7 +3,7 @@ import keras_metrics as km
 import keras.metrics as metrics
 from keras import optimizers
 from keras.models import Sequential
-from keras.layers import Conv3D, BatchNormalization, Activation
+from keras.layers import Conv3D
 from losses_and_metrics.keras_weighted_categorical_crossentropy import weighted_categorical_crossentropy
 from losses_and_metrics.dsc import dice_coef_label
 
@@ -11,23 +11,15 @@ from losses_and_metrics.dsc import dice_coef_label
 def six_conv_two_classes(input_shape, kernel_size, weights):
     # Input
     model = Sequential()
-    model.add(Conv3D(16, kernel_size=kernel_size, strides=(1, 1, 1), padding="same",
-                     input_shape=input_shape, use_bias=False))
-    model.add(BatchNormalization())
-    model.add(Activation("relu"))
-    model.add(Conv3D(16, kernel_size=kernel_size, strides=(1, 1, 1), padding="same",
-              use_bias=False))
-    model.add(BatchNormalization())
-    model.add(Activation("relu"))
-    model.add(Conv3D(16, kernel_size=kernel_size, strides=(1, 1, 1), padding="same",
-              use_bias=False))
-    model.add(BatchNormalization())
-    model.add(Activation("relu"))
+    model.add(Conv3D(16, kernel_size=kernel_size, strides=(1, 1, 1), activation='relu', padding="same",
+                     input_shape=input_shape))
+    model.add(Conv3D(16, kernel_size=kernel_size, strides=(1, 1, 1), activation='relu', padding="same"))
+    model.add(Conv3D(16, kernel_size=kernel_size, strides=(1, 1, 1), activation='relu', padding="same"))
     model.add(Conv3D(2, kernel_size=kernel_size, strides=(1, 1, 1), activation='softmax', padding="same"))
 
     # define optimizer
     # sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    adam = optimizers.Adam(lr=0.2, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
+    adam = optimizers.Adam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
 
     # define loss function
     loss_function = weighted_categorical_crossentropy(weights)
