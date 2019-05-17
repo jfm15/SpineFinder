163678fdd218d20385779040265eaae98a4359fe
jfm15 <jfm15@imperial.ac.uk>
2019-05-17 21:59:27 +0100

    use sgd instead

--- models/simple_identification.py
+++ models/simple_identification.py
@@ -23,8 +23,8 @@ def simple_identification(input_shape, kernel_size, filters, learning_rate):
 
     # NOTE: if any of the below parameters change then change the description file
     adam = optimizers.Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=1e-6)
-
-    model.compile(optimizer=adam, loss=ignore_background_loss, metrics=["mean_absolute_error"])
+    sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss=ignore_background_loss, metrics=["mean_absolute_error"])
 
     return model
 
