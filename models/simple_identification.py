c9d792c3eb086670d2526345f6b430219c6033e8
jfm15 <jfm15@imperial.ac.uk>
2019-05-17 21:46:45 +0100

    initalize with weights

--- models/simple_identification.py
+++ models/simple_identification.py
@@ -1,18 +1,18 @@
 from keras import optimizers
 from keras import backend as K
 from keras.models import Model, Sequential
+from keras.initializers import RandomNormal
 from keras.layers import Input, Conv2D, UpSampling2D, MaxPooling2D, concatenate
 
 
 def simple_identification(input_shape, kernel_size, filters, learning_rate):
     main_input = Input(shape=(None, None, 1))
-    x = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same")(main_input)
-    x = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same")(x)
-    x = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same")(x)
-    x = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same")(x)
-    x = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same")(x)
-    x = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same")(x)
-    x = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same")(x)
+    x = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same",
+               kernel_initializer=RandomNormal(mean=3.0))(main_input)
+    x = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same",
+               kernel_initializer=RandomNormal(mean=3.0))(x)
+    x = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same",
+               kernel_initializer=RandomNormal(mean=3.0))(x)
     main_output = Conv2D(1, kernel_size=kernel_size, strides=(1, 1), activation='relu', padding="same")(x)
 
     model = Model(inputs=main_input, outputs=main_output)
