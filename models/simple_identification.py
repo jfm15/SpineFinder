e6a90258fd5958a6bddbb3000eb0410ccd8f6041
jfm15 <jfm15@imperial.ac.uk>
2019-05-19 21:57:30 +0100

    reduce batch size

--- models/simple_identification.py
+++ models/simple_identification.py
@@ -143,8 +143,8 @@ def unet_slices(input_shape, kernel_size, filters, learning_rate):
     model = Model(inputs=main_input, outputs=main_output)
 
     # define optimizer
-    sgd = optimizers.SGD(lr=learning_rate, decay=1e-6, momentum=0.9)
-    # adam = optimizers.Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=1e-6)
-    model.compile(optimizer=sgd, loss=ignore_background_loss, metrics=[vertebrae_classification_rate])
+    # sgd = optimizers.SGD(lr=learning_rate, decay=1e-6, momentum=0.9)
+    adam = optimizers.Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=1e-6)
+    model.compile(optimizer=adam, loss=ignore_background_loss, metrics=[vertebrae_classification_rate])
 
     return model
