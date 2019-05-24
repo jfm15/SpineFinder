5a561e23dddfa4b16a312747799360d8e38026f8
jfm15 <jfm15@imperial.ac.uk>
2019-05-24 23:53:19 +0100

    fix

--- main_slice.py
+++ main_slice.py
@@ -8,7 +8,7 @@ model_params = {'kernel_size': (3, 3),
 
 perform_learning(sample_dir="samples/slices",
                  training_val_split=0.5,
-                 sample_shape=(272, 720),
+                 sample_shape=(80, 320),
                  batch_size=32,
                  sample_channels=1,
                  categorise=False,
