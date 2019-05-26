af2a50c213c840ba3700ef435066feb159b9b248
jfm15 <jfm15@imperial.ac.uk>
2019-05-26 04:11:21 +0100

    lower learning rate:

--- main_slice.py
+++ main_slice.py
@@ -3,8 +3,8 @@ from models.simple_identification import simple_identification, unet_slices
 
 # inputs to the model
 model_params = {'kernel_size': (3, 3),
-                'filters': 32,
-                'learning_rate': 0.001}
+                'filters': 16,
+                'learning_rate': 0.00075}
 
 perform_learning(sample_dir="samples/slices",
                  training_val_split=0.5,
