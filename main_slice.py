aa493310baffc779602f33fae2109a5484e24f32
jfm15 <jfm15@imperial.ac.uk>
2019-05-26 16:34:30 +0100

    rectangle filters

--- main_slice.py
+++ main_slice.py
@@ -2,14 +2,14 @@ from perform_learning import perform_learning
 from models.simple_identification import simple_identification, unet_slices
 
 # inputs to the model
-model_params = {'kernel_size': (3, 3),
+model_params = {'kernel_size': (3, 7),
                 'filters': 16,
                 'learning_rate': 0.001}
 
 perform_learning(sample_dir="samples/slices",
                  training_val_split=0.5,
                  sample_shape=(80, 320),
-                 batch_size=16,
+                 batch_size=32,
                  sample_channels=1,
                  categorise=False,
                  output_classes=1,
