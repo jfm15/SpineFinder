e2d847a8a05c23a3ce536aa45f71ec1dcba0a197
jfm15 <jfm15@imperial.ac.uk>
2019-05-26 04:26:32 +0100

    up momentum

--- models/simple_identification.py
+++ models/simple_identification.py
@@ -74,86 +74,86 @@ def unet_slices(input_shape, kernel_size, filters, learning_rate):
 
     # 80 x 320
     step_down_1 = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(main_input)
-    step_down_1 = BatchNormalization(momentum=0.1)(step_down_1)
+    step_down_1 = BatchNormalization()(step_down_1)
     step_down_1 = Activation("relu")(step_down_1)
     step_down_1 = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_down_1)
-    step_down_1 = BatchNormalization(momentum=0.1)(step_down_1)
+    step_down_1 = BatchNormalization()(step_down_1)
     step_down_1 = Activation("relu")(step_down_1)
 
     # 40 x 160
     step_down_2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(step_down_1)
     step_down_2 = Conv2D(2 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_down_2)
-    step_down_2 = BatchNormalization(momentum=0.1)(step_down_2)
+    step_down_2 = BatchNormalization()(step_down_2)
     step_down_2 = Activation("relu")(step_down_2)
     step_down_2 = Conv2D(2 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_down_2)
-    step_down_2 = BatchNormalization(momentum=0.1)(step_down_2)
+    step_down_2 = BatchNormalization()(step_down_2)
     step_down_2 = Activation("relu")(step_down_2)
 
     # 20 x 80
     step_down_3 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(step_down_2)
     step_down_3 = Conv2D(4 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_down_3)
-    step_down_3 = BatchNormalization(momentum=0.1)(step_down_3)
+    step_down_3 = BatchNormalization()(step_down_3)
     step_down_3 = Activation("relu")(step_down_3)
     step_down_3 = Conv2D(4 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_down_3)
-    step_down_3 = BatchNormalization(momentum=0.1)(step_down_3)
+    step_down_3 = BatchNormalization()(step_down_3)
     step_down_3 = Activation("relu")(step_down_3)
 
     # 10 x 40
     step_down_4 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(step_down_3)
     step_down_4 = Conv2D(8 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_down_4)
-    step_down_4 = BatchNormalization(momentum=0.1)(step_down_4)
+    step_down_4 = BatchNormalization()(step_down_4)
     step_down_4 = Activation("relu")(step_down_4)
     step_down_4 = Conv2D(8 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_down_4)
-    step_down_4 = BatchNormalization(momentum=0.1)(step_down_4)
+    step_down_4 = BatchNormalization()(step_down_4)
     step_down_4 = Activation("relu")(step_down_4)
 
     # 5 x 20
     floor = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(step_down_4)
     floor = Conv2D(16 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(floor)
-    floor = BatchNormalization(momentum=0.1)(floor)
+    floor = BatchNormalization()(floor)
     floor = Activation("relu")(floor)
     floor = Conv2D(16 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(floor)
-    floor = BatchNormalization(momentum=0.1)(floor)
+    floor = BatchNormalization()(floor)
     floor = Activation("relu")(floor)
 
     # 10 x 40
     step_up_4 = UpSampling2D(size=(2, 2))(floor)
     step_up_4 = concatenate([step_down_4, step_up_4], axis=-1)
     step_up_4 = Conv2D(8 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_up_4)
-    step_up_4 = BatchNormalization(momentum=0.1)(step_up_4)
+    step_up_4 = BatchNormalization()(step_up_4)
     step_up_4 = Activation("relu")(step_up_4)
     step_up_4 = Conv2D(8 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_up_4)
-    step_up_4 = BatchNormalization(momentum=0.1)(step_up_4)
+    step_up_4 = BatchNormalization()(step_up_4)
     step_up_4 = Activation("relu")(step_up_4)
 
     # 20 x 80
     step_up_3 = UpSampling2D(size=(2, 2))(step_up_4)
     step_up_3 = concatenate([step_down_3, step_up_3], axis=-1)
     step_up_3 = Conv2D(4 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_up_3)
-    step_up_3 = BatchNormalization(momentum=0.1)(step_up_3)
+    step_up_3 = BatchNormalization()(step_up_3)
     step_up_3 = Activation("relu")(step_up_3)
     step_up_3 = Conv2D(4 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_up_3)
-    step_up_3 = BatchNormalization(momentum=0.1)(step_up_3)
+    step_up_3 = BatchNormalization()(step_up_3)
     step_up_3 = Activation("relu")(step_up_3)
 
     # 40 x 160
     step_up_2 = UpSampling2D(size=(2, 2))(step_up_3)
     step_up_2 = concatenate([step_down_2, step_up_2], axis=-1)
     step_up_2 = Conv2D(2 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_up_2)
-    step_up_2 = BatchNormalization(momentum=0.1)(step_up_2)
+    step_up_2 = BatchNormalization()(step_up_2)
     step_up_2 = Activation("relu")(step_up_2)
     step_up_2 = Conv2D(2 * filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_up_2)
-    step_up_2 = BatchNormalization(momentum=0.1)(step_up_2)
+    step_up_2 = BatchNormalization()(step_up_2)
     step_up_2 = Activation("relu")(step_up_2)
 
     # 80 x 320
     step_up_1 = UpSampling2D(size=(2, 2))(step_up_2)
     step_up_1 = concatenate([step_down_1, step_up_1], axis=-1)
     step_up_1 = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_up_1)
-    step_up_1 = BatchNormalization(momentum=0.1)(step_up_1)
+    step_up_1 = BatchNormalization()(step_up_1)
     step_up_1 = Activation("relu")(step_up_1)
     step_up_1 = Conv2D(filters, kernel_size=kernel_size, strides=(1, 1), padding="same")(step_up_1)
-    step_up_1 = BatchNormalization(momentum=0.1)(step_up_1)
+    step_up_1 = BatchNormalization()(step_up_1)
     step_up_1 = Activation("relu")(step_up_1)
 
     main_output = Conv2D(1, kernel_size=(1, 1), strides=(1, 1), padding="same",
