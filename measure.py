29a4124fa2671ee6e3afda3c1762510681ca0037
jfm15 <jfm15@imperial.ac.uk>
2019-05-27 14:20:16 +0100

    debug statement

--- measure.py
+++ measure.py
@@ -31,6 +31,8 @@ def apply_detection_model(volume, model, X_size, y_size):
     volume_padded = np.pad(volume_padded, paddings, mode="constant")
     output = np.zeros(volume_padded.shape)
 
+    print(X_size, y_size, output.shape)
+
     for x in range(0, volume_padded.shape[0], y_size[0]):
         for y in range(0, volume_padded.shape[1], y_size[1]):
             for z in range(0, volume_padded.shape[2], y_size[2]):
