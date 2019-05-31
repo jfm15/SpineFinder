645d5a7ad75d14c3ba767a05f9e01697991201ec
jfm15 <jfm15@imperial.ac.uk>
2019-05-31 19:04:31 +0100

    go back

--- main_slice.py
+++ main_slice.py
@@ -4,11 +4,11 @@ from models.simple_identification import simple_identification, unet_slices
 # inputs to the model
 model_params = {'kernel_size': (3, 3),
                 'filters': 16,
-                'learning_rate': 0.00005}
+                'learning_rate': 0.0001}
 
 perform_learning(sample_dir="samples/slices",
                  training_val_split=0.5,
-                 batch_size=1,
+                 batch_size=8,
                  sample_channels=1,
                  categorise=False,
                  output_classes=1,
