5644a80c13c7fbb5023beb64cf4253b586cb769f
jfm15 <jfm15@imperial.ac.uk>
2019-06-01 17:26:18 +0100

    test

--- measure.py
+++ measure.py
@@ -143,9 +143,10 @@ def test_scan(scan_path, centroid_path, detection_model_path, detection_X_shape,
             arr = np.array(histogram[key])
             print(LABELS_NO_L6[key], arr.shape[0])
             if arr.shape[0] > 3000:
-                centroid_estimate = np.median(arr, axis=0)
-                print(arr.shape)
-                clustering = MeanShift().fit(arr)
+                # centroid_estimate = np.median(arr, axis=0)
+                ms = MeanShift()
+                ms.fit(arr)
+                print(ms.cluster_centers_)
                 centroid_estimate = np.around(centroid_estimate, decimals=2)
                 labels.append(LABELS_NO_L6[key])
                 centroid_estimates.append(list(centroid_estimate))
