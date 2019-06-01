40b5f0d252f58157ada4ab86771ac1462d8e4d6c
jfm15 <jfm15@imperial.ac.uk>
2019-06-01 18:00:14 +0100

    speed up

--- measure.py
+++ measure.py
@@ -144,7 +144,7 @@ def test_scan(scan_path, centroid_path, detection_model_path, detection_X_shape,
             print(LABELS_NO_L6[key], arr.shape[0])
             if arr.shape[0] > 3000:
                 centroid_estimate = np.median(arr, axis=0)
-                ms = MeanShift()
+                ms = MeanShift(min_bin_freq=3000)
                 ms.fit(arr)
                 print(ms.cluster_centers_)
                 centroid_estimate = np.around(centroid_estimate, decimals=2)
