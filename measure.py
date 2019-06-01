c6961f60fbcc8873edc70155b2b4403cb18de188
jfm15 <jfm15@imperial.ac.uk>
2019-06-01 18:53:02 +0100

    use mean shift

--- measure.py
+++ measure.py
@@ -38,7 +38,6 @@ def apply_detection_model(volume, model, X_size, y_size):
     for x in range(0, volume_padded.shape[0] - X_size[0] + 1, y_size[0]):
         for y in range(0, volume_padded.shape[1] - X_size[1] + 1, y_size[1]):
             for z in range(0, volume_padded.shape[2] - X_size[2] + 1, y_size[2]):
-                print(x, y, z)
                 corner_a = [x, y, z]
                 corner_b = corner_a + X_size
                 corner_c = corner_a + border
@@ -143,10 +142,10 @@ def test_scan(scan_path, centroid_path, detection_model_path, detection_X_shape,
             arr = np.array(histogram[key])
             print(LABELS_NO_L6[key], arr.shape[0])
             if arr.shape[0] > 1500:
-                centroid_estimate = np.median(arr, axis=0)
+                #centroid_estimate = np.median(arr, axis=0)
                 ms = MeanShift(bin_seeding=True, min_bin_freq=300)
                 ms.fit(arr)
-                print(ms.cluster_centers_)
+                centroid_estimate = ms.cluster_centers_[0]
                 centroid_estimate = np.around(centroid_estimate, decimals=2)
                 labels.append(LABELS_NO_L6[key])
                 centroid_estimates.append(list(centroid_estimate))
