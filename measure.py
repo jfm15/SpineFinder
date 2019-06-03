045240a819e675e3756e22681196a5870a67e26e
jfm15 <jfm15@imperial.ac.uk>
2019-06-03 15:47:09 +0100

    correct path

--- measure.py
+++ measure.py
@@ -413,5 +413,5 @@ complete_identification_picture('datasets_test', 'saved_current_models/detec-15:
                                 spacing=(1.0, 1.0, 1.0))
 '''
 
-get_stats('datasets_test', 'saved_current_models/detec-20:06.h5',
+get_stats('spine-test-data', 'saved_current_models/detec-20:06.h5',
           'saved_current_models/ident-10:20.h5', spacing=(1.0, 1.0, 1.0))
