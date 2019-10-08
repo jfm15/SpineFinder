e7177d263061e38317c2a4f74dcf852ff10379d3
jfm15 <jfm15@imperial.ac.uk>
2019-10-08 17:48:37 +0100

    fix arguments

--- train_identification_model.py
+++ train_identification_model.py
@@ -7,8 +7,8 @@ model_params = {'kernel_size': (3, 3),
                 'filters': 32,
                 'learning_rate': 0.001}
 
-perform_learning(training_sample_dir=sys.argv[0],
-                 val_sample_dir=sys.argv[1],
+perform_learning(training_sample_dir=sys.argv[1],
+                 val_sample_dir=sys.argv[2],
                  batch_size=32,
                  three_d=False,
                  sample_channels=8,
@@ -18,6 +18,6 @@ perform_learning(training_sample_dir=sys.argv[0],
                  model_func=identification_unet,
                  model_params=model_params,
                  epochs=35,
-                 model_path=sys.argv[2],
+                 model_path=sys.argv[3],
                  checkpoint_path="checkpoints/slices_model/slices_model.{epoch:02d}.h5",
                  log_name="identification")
